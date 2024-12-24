074d7e77d4a3abd4bf87696c1771c8b39407dba6
Amit Kumar <amitkumar790194@gmail.com>
2024-12-25 05:18:47 +0530

    identify character and blinking underline on current character functionality done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const typingText = document.querySelector(".typing-text p");
+inpField = document.querySelector(".wrapper .input-field");
+let charIndex = 0;
 
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length);
@@ -6,7 +8,25 @@ function randomParagraph() {
     let spanTag = `<span>${char}</span>`;
     typingText.innerHTML += spanTag;
   });
-  console.log(paragraphs);
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
 }
 
 randomParagraph();
+
+function initTyping() {
+  const characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (characters[charIndex].innerText === typedChar) {
+    characters[charIndex].classList.add("correct");
+    console.log("correct");
+  } else {
+    characters[charIndex].classList.add("incorrect");
+    console.log("incorrect");
+  }
+  charIndex++;
+  characters.forEach((span) => span.classList.remove("active"));
+  characters[charIndex].classList.add("active");
+}
+
+inpField.addEventListener("input", initTyping);
